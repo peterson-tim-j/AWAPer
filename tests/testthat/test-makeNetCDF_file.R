8283673593d78f6d2a23d772308171579daaeaf7
Tim Peterson <tim.peterson@monash.edu>
2025-11-22 19:57:50 +1100

    Testthat bug fixed

--- tests/testthat/test-makeNetCDF_file.R
+++ tests/testthat/test-makeNetCDF_file.R
@@ -2,6 +2,10 @@
 test_that("netCDF grid can be created",
     {
 
+      Sys.setenv(R_TESTS="")
+
+      x = 10
+      expect_true(x>1,'Dummp test')
       expect_no_error(
         {
           # Set dates for building netCDFs and extracting data from yesterday to one week ago.
@@ -10,7 +14,7 @@ test_that("netCDF grid can be created",
 
           # define temp direcory for netCDF files
           fdir = tempdir()
-          setwd(fdir)
+          #setwd(fdir)
 
           # Set names for netCDF files (in the system temp. directory).
           ncdfFilename = file.path(fdir,'data.nc')
@@ -24,7 +28,6 @@ test_that("netCDF grid can be created",
         message='Testing creaion of netCDF grids.'
       )
 
-
       # Test the files were created
       expect_true(file.exists(ncdfFilename),'Testing creation of non-solar netCDF file for data from one week ago')
       expect_true(file.exists(ncdfSolarFilename),'Testing creation of solar netCDF file for data from one week ago')
@@ -33,13 +36,18 @@ test_that("netCDF grid can be created",
       expect_gt(file.info(ncdfFilename)$size,1E6,'Testing non-solar netCDF file size is > 1MB')
       expect_gt(file.info(ncdfSolarFilename)$size,1E6,'Testing solar netCDF file size is > 1MB')
 
-      # Test the netcDF files can be opened
-      expect_s3_class( ncdf4::nc_open(ncdfFilename, write=T), 'ncdf4')
-      expect_s3_class( ncdf4::nc_open(ncdfSolarFilename, write=T), 'ncdf4')
+      # Test the netcdf files can be opened.
+      expect_no_error(
+        {
+          ncout <- ncdf4::nc_open(ncdfFilename, write=T)
+          ncout.solar <- ncdf4::nc_open(ncdfSolarFilename, write=T)
+        },
+        message='Testing opening of netCDF grids.'
+      )
 
-      # Open the netcdf files and check the dimensions
-      ncout <- ncdf4::nc_open(ncdfFilename, write=T)
-      ncout.solar <- ncdf4::nc_open(ncdfSolarFilename, write=T)
+      # Test the netcDF files can be opened
+      expect_s3_class( ncout, 'ncdf4')
+      expect_s3_class( ncout.solar, 'ncdf4')
 
       # check the dimension names
       expect_true(ncdf4::ncatt_get(ncout, "time")$long_name == "time", "Testing time dimension exists in netCDF file")
@@ -56,18 +64,25 @@ test_that("netCDF grid can be created",
       expect_length(ncout.solar$dim$Long$vals, 839)
 
       # Update netcDF grids and expect no errors
-      endDate = startDate
-      startDate = Sys.Date()-11
       expect_no_error(
-        makeNetCDF_file(ncdfFilename=ncdfFilename,
-                        ncdfSolarFilename=ncdfSolarFilename,
-                        updateFrom=startDate, updateTo=endDate),
+        {
+          endDate = startDate
+          startDate = Sys.Date()-11
+          makeNetCDF_file(ncdfFilename=ncdfFilename,
+                          ncdfSolarFilename=ncdfSolarFilename,
+                          updateFrom=startDate, updateTo=endDate)
+        },
         message='Testing updating of netCDF grids by two days prior'
       )
 
-      # Delete temp files and folder
-      unlink(ncdfFilename)
-      unlink(ncdfSolarFilename)
-      unlink(fdir, recursive = TRUE)
+      # # Delete temp files and folder
+      # expect_no_error(
+      #   {
+      #     unlink(ncdfFilename)
+      #     unlink(ncdfSolarFilename)
+      #     unlink(fdir, recursive = TRUE)
+      #   },
+      #   message='Removing files created during test'
+      # )
     }
 )
